15afac6d284bc4cde253c960e240708e2ffcd1b5
rel1cta <dimksha036@gmail.com>
2022-11-08 19:15:06 +0200

    lab5.1

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y);
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double c = pow(h(abs(p-q), sqrt(abs(p-q))), 2) + h(1, pow(q,2) - pow(p,2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y)
+{
+	return (pow(y,2) - sqrt(pow(x,2)+pow(y,2)))/(1 + pow(x,2)*pow(y,2));
+}
